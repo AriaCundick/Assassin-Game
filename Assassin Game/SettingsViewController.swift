70e91e63b758cf6e2b1b92b16d02dafeb58931b9
Aria Cundick <aria.cundick@gmail.com>
2015-06-12 17:55:11 -0500

    working change name button

--- Assassin Game/SettingsViewController.swift
+++ Assassin Game/SettingsViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 
 class SettingsViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
+    //MARK - IBOutlet
     //pointer to the UIImgview in settings page
     @IBOutlet var imageView: UIImageView!
+    @IBOutlet var userName: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +26,8 @@ class SettingsViewController: UIViewController, UIImagePickerControllerDelegate,
         // Dispose of any resources that can be recreated.
     }
     
+    //MARK - IBAction
+    
     //hook up this action in the first responder to the re-take photo button
     //uses camera to take a new photo
     @IBAction func chooseImageFromCamera(){
@@ -34,10 +38,41 @@ class SettingsViewController: UIViewController, UIImagePickerControllerDelegate,
         
         presentViewController(picker, animated: true, completion: nil)
     }
-
+    
+    //save image to uiimage
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
         imageView.image = (info[UIImagePickerControllerOriginalImage] as! UIImage)
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    @IBAction func changeNameButton(sender: AnyObject) {
+        //pop up box
+        var alert = UIAlertController(title: "Change Name", message: "Enter a new name", preferredStyle: .Alert)
+        //save button
+        let saveAction = UIAlertAction(title: "Change", style: .Default )
+            { (action) -> Void in
+                
+                var text1 = alert.textFields![0] as! UITextField
+                if text1.text == "" 
+                {
+                    text1.text = self.userName.text!
+                }
+                
+                self.userName.text = text1.text
+                self.userName.reloadInputViews()
+            }
+        
+        //cancel button
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Default, handler: nil)
+        
+        alert.addTextFieldWithConfigurationHandler(nil)
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+       
+        presentViewController(alert, animated: true, completion: nil)
+        
+        
     }
    
 
